6ac249f31f6fd0889c8881b61be78af46d6ebcf3
heesane <tees3359@gmail.com>
2024-12-19 00:25:20 +0900

    refactor : 각 tf 파일별 comment

--- instance.tf
+++ instance.tf
@@ -21,12 +21,17 @@ resource "google_compute_instance" "default" {
         # VM Instance 이미지 지정
         initialize_params {
             image  = "ubuntu-os-cloud/ubuntu-2004-lts"
-            size   = 25  # 25 GB disk size
+            # 25 GB disk size
+            # 기본적으로 os 이미지의 사이즈는 5GB가량
+            # 부팅 이미지를 설치하고 남는 잉여 디스크는 사용 가능한 용량으로 전환
+            # 별도의 디스크를 attach하고 싶다면, 별도의 디스크를 생성하고, attach해야함
+            size   = 25  
             type   = "pd-balanced"
         }
     }
 
     # VM Instance에 부팅 시 실행할 스크립트
+    # git clone을 통해서, 필요한 소스코드나 jar를 통해서 간단하게 실행 가능
     metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential git; echo 'Hello, World!' > test.txt"
 
     # VM Instance에 할당할 네트워크 인터페이스
